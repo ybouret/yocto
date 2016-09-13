9ede4e3c9111040f4ea76eef7a60db62a36789e9
ybouret <yann.bouret@gmail.com>
2016-09-13 13:01:53 +0200

    WIP clean

--- clean.cmake
+++ clean.cmake
@@ -1,7 +1,18 @@
-FILE(GLOB to_remove *.dat)
-MESSAGE("to_remove: ${to_remove}")
+SET(to_remove "")
+
+FUNCTION(__ADD_PATTERN pattern)
+	FILE(GLOB tmp ${pattern})
+	SET(to_remove ${to_remove} ${tmp} PARENT_SCOPE)
+ENDFUNCTION(__ADD_PATTERN)
+
+__ADD_PATTERN(*.dat)
+__ADD_PATTERN(*.bin)
+__ADD_PATTERN(*.png)
+__ADD_PATTERN(*.jpg)
+
 FOREACH(item IN LISTS to_remove)
 	MESSAGE("will remove ${item}")
+	EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E remove -f ${item})
 ENDFOREACH(item)
 
 
