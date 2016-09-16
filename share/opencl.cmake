80fe7d921f6b5de9fda31ca7f2478421dbbec9d1
ybouret <yann.bouret@gmail.com>
2016-09-16 08:53:09 +0200

    start ocl...

---
+++ share/opencl.cmake
@@ -0,0 +1,27 @@
+########################################################################
+##
+##
+## Detecting OpenCL's env:
+##                    -- set    YOCTO_OCL_FOUND
+##                    -- define YOCTO_OCL_LINK_TO
+##
+########################################################################
+SET(YOCTO_OCL_FOUND OFF)
+MESSAGE( STATUS "[OpenCL] Detecting Installation..." )
+
+########################################################################
+##
+##
+##
+########################################################################
+IF(YOCTO_DARWIN)
+	MESSAGE( STATUS "[OpenCL] Using Apple OpenCL" )
+	SET(YOCTO_OCL_FOUND ON)
+	
+	MACRO(YOCTO_OCL_LINK_TO tgt)
+		MESSAGE( STATUS "${tgt} will use OpenCL (Apple)" )
+		TARGET_LINK_LIBRARIES( ${tgt} "-framework OpenCL")
+	ENDMACRO(YOCTO_OCL_LINK_TO)
+ENDIF()
+
+
---
+++ src/ocl/tests/test-types.cpp
@@ -0,0 +1,10 @@
+#include "yocto/ocl/types.hpp"
+#include "yocto/utest/run.hpp"
+
+using namespace yocto;
+
+YOCTO_UNIT_TEST_IMPL(types)
+{
+	
+}
+YOCTO_UNIT_TEST_DONE()
