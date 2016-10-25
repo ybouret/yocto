d0ee53a40def63f8bd6512d5700edd19e3f9e25d
ybouret <yann.bouret@gmail.com>
2016-10-25 11:52:41 +0200

    WIP thread server...

--- src/yocto/yocto/threading/scheme/server.cpp
+++ src/yocto/yocto/threading/scheme/server.cpp
@@ -38,3 +38,36 @@ namespace yocto
         }
     }
 }
+
+
+namespace yocto
+{
+    namespace threading
+    {
+        par_server:: task:: ~task() throw()
+        {
+        }
+
+        par_server:: task:: task(const job_id j, const kernel &k) :
+        next(0),
+        prev(0),
+        uuid(j),
+        proc(k)
+        {
+        }
+
+        par_server:: ~par_server() throw()
+        {
+        }
+
+        par_server:: par_server(const bool setVerbose) :
+        par_executor(setVerbose),
+        server( *static_cast<par_executor *>(this) ),
+        pending(),
+        current(),
+        storage()
+        {
+        }
+
+    }
+}
--- src/yocto/yocto/threading/scheme/server.hpp
+++ src/yocto/yocto/threading/scheme/server.hpp
@@ -9,7 +9,10 @@ namespace yocto
     {
         typedef uint32_t job_id;
 
+        //______________________________________________________________________
+        //
         //! server interface
+        //______________________________________________________________________
         class server
         {
         public:
@@ -28,7 +31,10 @@ namespace yocto
             YOCTO_DISABLE_COPY_AND_ASSIGN(server);
         };
 
-
+        //______________________________________________________________________
+        //
+        //! simple sequential server....
+        //______________________________________________________________________
         class seq_server : public seq_executor, public server
         {
         public:
@@ -41,6 +47,46 @@ namespace yocto
             YOCTO_DISABLE_COPY_AND_ASSIGN(seq_server);
         };
 
+
+        //______________________________________________________________________
+        //
+        //! parallel server
+        //______________________________________________________________________
+        class par_server : public par_executor, public server
+        {
+        public:
+            virtual ~par_server() throw();
+            explicit par_server(const bool setVerbose=false);
+
+
+
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(par_server);
+            static void control_loop( void *args ) throw();
+            static void workers_loop( void *args ) throw();
+
+            class task : public object
+            {
+            public:
+                task         *next;
+                task         *prev;
+                const  job_id uuid;
+                kernel        proc;
+
+                task(const job_id j, const kernel &k);
+                ~task() throw();
+
+            private:
+                YOCTO_DISABLE_COPY_AND_ASSIGN(task);
+            };
+
+            typedef core::pool_of<task>     task_pool; //!< dead tasks
+            typedef core::pool_of_cpp<task> task_list; //!< alive tasks
+            task_list pending; //!< tasks to be done
+            task_list current; //!< tasks being processed
+            task_pool storage; //!< cache
+
+        };
 
     }
 }
