ac1f7488e3569e27052fd59f6735cdc0df4ed105
ybouret <yann.bouret@gmail.com>
2017-05-02 11:34:53 +0200

    layouts WIP

--- src/spade/tests/test-layout.cpp
+++ src/spade/tests/test-layout.cpp
@@ -9,5 +9,20 @@ YOCTO_UNIT_TEST_IMPL(layout)
     std::cerr << "sizeof(layout1D)=" << sizeof(layout1D) << std::endl;
     std::cerr << "sizeof(layout2D)=" << sizeof(layout2D) << std::endl;
     std::cerr << "sizeof(layout3D)=" << sizeof(layout3D) << std::endl;
+
+    {
+        layout1D L(10,-1);
+        std::cerr << "Layout1D=" << L << std::endl;
+    }
+
+    {
+        layout2D L( coord2D(5,3), coord2D(-3,-2) );
+        std::cerr << "Layout2D=" << L << std::endl;
+    }
+
+    {
+        layout3D L( coord3D(5,3,-4), coord3D(-3,-2,8) );
+        std::cerr << "Layout3D=" << L << std::endl;
+    }
 }
 YOCTO_UNIT_TEST_DONE()
--- src/spade/yocto/spade/types.hpp
+++ src/spade/yocto/spade/types.hpp
@@ -80,4 +80,5 @@ namespace yocto
     }
 }
 
+
 #endif
