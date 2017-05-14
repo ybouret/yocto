0ac98f2a45d74f34c32c3d33ab21e81524ea204d
ybouret <yann.bouret@gmail.com>
2017-05-14 11:24:31 +0200

    will make a generic mpi_split

---
+++ src/mpi/yocto/mpi/split.hpp
@@ -0,0 +1,14 @@
+#ifndef YOCTO_MPI_SPLIT_INCLUDED
+#define YOCTO_MPI_SPLIT_INCLUDED 1
+
+#include "yocto/math/point3d.hpp"
+
+namespace yocto
+{
+    struct mpi_split
+    {
+        
+    };
+}
+
+#endif
