16d5dafcb1327d2d4558f40b7fa1f32a2f609f53
ybouret <yann.bouret@gmail.com>
2017-05-31 12:23:05 +0200

    updated test

--- src/spade/tests/test-dispatch.cpp
+++ src/spade/tests/test-dispatch.cpp
@@ -9,21 +9,23 @@ YOCTO_UNIT_TEST_IMPL(dispatch)
 {
     if(argc>1)
     {
-        const size_t cpus = max_of<size_t>(1, atol(argv[1]) );
-        std::cerr << "splitting #cpus=" << cpus << std::endl;
-
+        for(int i=1;i<argc;++i)
         {
-            const Layout2D Lsq( coord2D(1,1), coord2D(256,256) );
-            const coord2D  nsq = Split::DispatchCPUs(Lsq,cpus);
-            std::cerr <<  "nsq=" << nsq << std::endl;
+            const size_t cpus = max_of<size_t>(1, atol(argv[i]) );
+            std::cerr << "splitting #cpus=" << cpus << std::endl;
+            
+            {
+                const Layout2D Lsq( coord2D(1,1), coord2D(256,256) );
+                const coord2D  nsq = Split::DispatchCPUs(Lsq,cpus);
+                std::cerr <<  "nsq=" << nsq << std::endl;
+            }
+            
+            {
+                const Layout3D Lsq( coord3D(1,1,1), coord3D(256,256,256) );
+                const coord3D  nsq = Split::DispatchCPUs(Lsq,cpus);
+                std::cerr <<  "nsq=" << nsq << std::endl;
+            }
         }
-        
-        {
-            const Layout3D Lsq( coord3D(1,1,1), coord3D(256,256,256) );
-            const coord3D  nsq = Split::DispatchCPUs(Lsq,cpus);
-            std::cerr <<  "nsq=" << nsq << std::endl;
-        }
-        
         
     }
 }
