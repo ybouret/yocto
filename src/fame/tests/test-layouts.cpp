bc99eea4b6187939d638e678f39db6f472427e12
ybouret <yann.bouret@gmail.com>
2017-06-02 09:56:49 +0200

    building layouts seems ok

--- src/fame/tests/test-layouts.cpp
+++ src/fame/tests/test-layouts.cpp
@@ -4,6 +4,15 @@
 using namespace yocto;
 using namespace fame;
 
+template <typename COORD>
+static inline
+void display_lay( const layouts_of<COORD> &L )
+{
+    std::cerr << L.full.sizes << "." << L.self.ranks << std::endl;
+    std::cerr << "\tinner=" << L.inner << std::endl;
+    std::cerr << "\touter=" << L.outer << std::endl;
+}
+
 YOCTO_UNIT_TEST_IMPL(layouts)
 {
     {
@@ -18,6 +27,7 @@ YOCTO_UNIT_TEST_IMPL(layouts)
             {
                 const domain_of<coord1d> D(rank,size,NULL,L,1);
                 layouts_of<coord1d>      W(D,1);
+                display_lay(W);
             }
             
             std::cerr << std::endl;
@@ -26,10 +36,68 @@ YOCTO_UNIT_TEST_IMPL(layouts)
             {
                 domain_of<coord1d>  D(rank,size,NULL,L,0);
                 layouts_of<coord1d> W(D,1);
+                display_lay(W);
             }
             
         }
     }
+    
+    
+    {
+        layout_of<coord2d> L( coord2d(1,1), coord2d(16,16) );
+        for(size_t size=1;size<=4;++size)
+        {
+            std::cerr << std::endl;
+            std::cerr << "size=" << size << std::endl;
+            
+            std::cerr << "IS  periodic:" << std::endl;
+            for(size_t rank=0;rank<size;++rank)
+            {
+                domain_of<coord2d>  D(rank,size,NULL,L,coord2d(1,1));
+                layouts_of<coord2d> W(D,1);
+                display_lay(W);
+            }
+            
+            std::cerr << "NOT periodic:" << std::endl;
+            for(size_t rank=0;rank<size;++rank)
+            {
+                domain_of<coord2d> D(rank,size,NULL,L,coord2d(0,0));
+                layouts_of<coord2d> W(D,1);
+                display_lay(W);
+
+            }
+            
+        }
+    }
+
+    {
+        layout_of<coord3d> L( coord3d(1,1,1), coord3d(16,16,16) );
+        for(size_t size=1;size<=8;++size)
+        {
+            std::cerr << std::endl;
+            std::cerr << "size=" << size << std::endl;
+            
+            std::cerr << "IS  periodic:" << std::endl;
+            for(size_t rank=0;rank<size;++rank)
+            {
+                domain_of<coord3d> D(rank,size,NULL,L,coord3d(1,1,1));
+                layouts_of<coord3d> W(D,1);
+                display_lay(W);
+            }
+            
+            std::cerr << "NOT periodic:" << std::endl;
+            for(size_t rank=0;rank<size;++rank)
+            {
+                domain_of<coord3d> D(rank,size,NULL,L,coord3d(0,0,0));
+                layouts_of<coord3d> W(D,1);
+                display_lay(W);
+            }
+            
+        }
+    }
+
+    
+    
 
 }
 YOCTO_UNIT_TEST_DONE()
--- src/fame/yocto/fame/layouts.hpp
+++ src/fame/yocto/fame/layouts.hpp
@@ -5,15 +5,17 @@
 
 namespace yocto
 {
-
+    
     namespace fame
     {
         template <typename COORD>
         class layouts_of : public domain_of<COORD>
         {
         public:
-            typedef domain_of<COORD> domain_type;
-            typedef layout_of<COORD> layout_type;
+            YOCTO_FAME_DECL_COORD;
+            typedef domain_of<COORD>           domain_type;
+            typedef layout_of<COORD>           layout_type;
+            typedef typename domain_type::link link;
             
             const layout_type outer;
             
@@ -22,16 +24,41 @@ namespace yocto
             inline layouts_of(const domain_type &dom,
                               const coord1d      num_ghosts) :
             domain_type(dom),
-            outer(this->inner)
+            outer( __expand(this->inner,*this,num_ghosts) )
             {
             }
             
             
         private:
             YOCTO_DISABLE_ASSIGN(layouts_of);
+            static inline
+            layout_type __expand(const layout_type &L,
+                                 const domain_type &D,
+                                 coord1d           num_ghosts)
+            {
+                if(num_ghosts<0) num_ghosts=0;
+                coord Lower = L.lower;
+                coord Upper = L.upper;
+                for(size_t dim=0;dim<DIMENSION;++dim)
+                {
+                    coord1d     &lower = __coord(Lower,dim);
+                    coord1d     &upper = __coord(Upper,dim);
+                    const link  &lnk   = D.links[dim];
+                    if(lnk.prev)
+                    {
+                        lower -= num_ghosts;
+                    }
+                    if(lnk.next)
+                    {
+                        upper += num_ghosts;
+                    }
+                }
+                
+                return layout_type(Lower,Upper);
+            }
         };
     }
-
+    
 }
 
 #endif
