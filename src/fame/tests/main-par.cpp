d386858ac56c708e819f74a05e13eb9eb23b816d
ybouret <yann.bouret@gmail.com>
2017-06-12 11:58:21 +0200

    need to redefine ghosts...

--- src/fame/tests/main-par.cpp
+++ src/fame/tests/main-par.cpp
@@ -1,42 +1,99 @@
-#include "yocto/fame/ghost.hpp"
-#include "yocto/fame/layouts.hpp"
+#include "yocto/fame/ghost-io.hpp"
+#include "yocto/fame/field1d.hpp"
 #include "yocto/program.hpp"
 #include "yocto/mpi/mpi.hpp"
+#include "yocto/container/cslot.hpp"
 
 using namespace yocto;
 using namespace fame;
 
 
+static size_t ng = 2;
+
+template <typename FIELD>
+static inline
+void test_field_io(const ghost &g, const FIELD &F )
+{
+    const size_t gs  = g.size();
+    const size_t bpp = F.item_size;
+
+    cslot cmem(gs*bpp);
+
+    {
+        uint8_t *p = (uint8_t *)cmem.data;
+        ghostIO  gIO;
+        gIO.load(F,g,p);
+    }
+}
+
+template <typename COORD, typename FIELD>
+static inline
+void test_ghosts(const ghosts_of<COORD> &GG, const FIELD &F)
+{
+    mpi & MPI = * mpi::location();
+    MPI.Printf0(stderr, "testing with ITEM_SIZE=%d\n", unsigned(sizeof(typename FIELD::type)) );
+    for(size_t dim=0;dim<FIELD::DIMENSION;++dim)
+    {
+        const ghosts &G = GG[dim];
+        if(G.prev)
+        {
+            MPI.Printf(stderr,"prev: %u data\n", unsigned(G.prev->size()));
+            test_field_io(* G.prev, F);
+        }
+        else
+        {
+            MPI.Printf(stderr,"prev: no data\n");
+        }
+    }
+}
 
 YOCTO_PROGRAM_START()
 {
     YOCTO_MPI_ENV();
 
+    const layout<coord1d> full(-10,10);
+
     {
-        const layout<coord1d> full(-10,10);
         MPI.Printf0(stderr,"IS  periodic\n");
         const domain<coord1d> D(MPI.CommWorldRank,MPI.CommWorldSize,NULL,full,1);
-        layouts<coord1d>      W(D,2);
-        ghosts_of<coord1d>       G(W);
+        layouts<coord1d>      W(D,ng);
+        ghosts_of<coord1d>    G(W);
+
+
+
         MPI.Printf(stderr,"is %2ld.%2ld: layout: inner=[%5ld:%5ld], outer=[%5ld:%5ld]\n",
-                   D.full.size,D.self.rank,
-                   W.inner.lower,
-                   W.inner.upper,
-                   W.outer.lower,
-                   W.outer.upper);
+                   long(D.full.size),long(D.self.rank),
+                   long(W.inner.lower),
+                   long(W.inner.upper),
+                   long(W.outer.lower),
+                   long(W.outer.upper)
+                   );
+
+        const field1d<char>  Fi("Fi",D,ng);
+        const field1d<float> Ff("Ff",D,ng);
+        const field1d< point3d<double> > Fv3("Fv3",D,ng);
+
+        test_ghosts(G,Fi);
+        test_ghosts(G,Ff);
+        test_ghosts(G,Fv3);
+    }
 
 
-#if 0
-        std::cerr << "NOT periodic:" << std::endl;
-        for(size_t rank=0;rank<size;++rank)
-        {
-            domain_of<coord1d>  D(rank,size,NULL,L,0);
-            layouts_of<coord1d> W(D,1);
-            display_lay(W);
-            ghosts_of<coord1d> G(W);
-        }
-#endif
+    {
+        MPI.Printf0(stderr,"\nNOT  periodic\n");
+        const domain<coord1d> D(MPI.CommWorldRank,MPI.CommWorldSize,NULL,full,0);
+        layouts<coord1d>      W(D,ng);
+        ghosts_of<coord1d>    G(W);
 
+
+
+        MPI.Printf(stderr,"is %2ld.%2ld: layout: inner=[%5ld:%5ld], outer=[%5ld:%5ld]\n",
+                   long(D.full.size),long(D.self.rank),
+                   long(W.inner.lower),
+                   long(W.inner.upper),
+                   long(W.outer.lower),
+                   long(W.outer.upper)
+                   );
     }
 }
 YOCTO_PROGRAM_END()
--- src/fame/yocto/fame/ghost-io.hpp
+++ src/fame/yocto/fame/ghost-io.hpp
@@ -16,16 +16,33 @@ namespace yocto
             virtual ~ghostIO() throw();
 
             template <typename FIELD>
-            inline void read_from( const FIELD &F, const ghost &g, uint8_t * &p )
+            inline void load( const FIELD &F, const ghost &g, uint8_t * &p )
             {
+                assert(p);
                 for(size_t i=g.size();i>0;--i)
                 {
                     const size_t idx = g[i];
                     const void  *ptr = & F.entry[ idx ];
-                    bmove<sizeof( typename FIELD::type )>(p,ptr);
+                    core::bmove<sizeof( typename FIELD::type )>(p,ptr);
                     p +=  sizeof( typename FIELD::type );
                 }
             }
+
+            template <typename FIELD>
+            inline void save(FIELD &F, const  ghost &g, const uint8_t * &p )
+            {
+                assert(p);
+                for(size_t i=g.size();i>0;--i)
+                {
+                    const size_t idx = g[i];
+                    void        *ptr = & F.entry[ idx ];
+                    core::bmove<sizeof( typename FIELD::type )>(ptr,p);
+                    p +=  sizeof( typename FIELD::type );
+                }
+            }
+
+
+            
 
         private:
             size_t capacity;
