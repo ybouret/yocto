12fe8a9124711c570e16a904dfe3d3312083bd22
ybouret <yann.bouret@gmail.com>
2017-06-20 13:13:08 +0200

    code cleanup ok

--- src/fame/yocto/fame/mpi.hpp
+++ src/fame/yocto/fame/mpi.hpp
@@ -61,32 +61,12 @@ namespace yocto
             }
 
 
-            template <typename T>
-            inline void send(const ghosts_pair    &gp,
-                             const ghosts_io      &gio,
-                             const field<T,COORD> &F)
-            {
-                const size_t       ns = gio.load_into_send(gp,F);
-                MPI.Send(gio.send_addr(),ns, MPI_BYTE, gp.rank,0,MPI_COMM_WORLD);
-            }
-
-            template <typename T>
-            inline void recv(const ghosts_pair    &gp,
-                             const ghosts_io      &gio,
-                             field<T,COORD>       &F)
-            {
-                MPI_Status status;
-                const size_t nr = gp.size * sizeof(T);
-                MPI.Recv(gio.recv_addr(),nr,MPI_BYTE,gp.rank,0,MPI_COMM_WORLD,status);
-                gio.save_from_recv(gp,F);
-            }
-
 
             template <typename T>
             inline void perform( const ghosts_of<COORD> &Ghosts, field<T,COORD> &F )
             {
 
-                MPI.Printf0(stderr, "-- Perform Exchange\n");
+                //MPI.Printf0(stderr, "-- Perform Exchange\n");
                 slots_of<ghosts_io> &GhostsIO = *this;
 
                 //______________________________________________________________
@@ -95,7 +75,7 @@ namespace yocto
                 // first pass: local exchange
                 //
                 //______________________________________________________________
-                MPI.Printf0(stderr,"--\t#lcopy=%u\n", unsigned(Ghosts.num_lcopy));
+                //MPI.Printf0(stderr,"--\t#lcopy=%u\n", unsigned(Ghosts.num_lcopy));
                 for(size_t i=0;i<Ghosts.num_lcopy;++i)
                 {
                     const ghosts *g = Ghosts.lcopy[i];
@@ -110,36 +90,38 @@ namespace yocto
                 // asynchronous
                 //
                 //______________________________________________________________
-                MPI.Printf0(stderr,"--\t#async=%u\n", unsigned(Ghosts.num_async));
+                //MPI.Printf0(stderr,"--\t#async=%u\n", unsigned(Ghosts.num_async));
                 MPI_Status status;
                 //______________________________________________________________
                 //
                 // pass0: send to prev, recv from next
                 // pass1: send to next, recv from prev
                 //______________________________________________________________
-
                 for(int pass=0;pass<2;++pass)
                 {
-                    MPI.Printf0(stderr, "\t PASS #%d\n", pass+1);
+                    //MPI.Printf0(stderr, "\t PASS #%d\n", pass+1);
                     for(size_t dim=0;dim<DIMENSION;++dim)
                     {
 
                         const ghosts &g    = Ghosts[dim];
-                        const char   *kind = g.kind_text();
-                        const char   *axID = coord_info::axis_name(dim);
+                        //const char   *kind = g.kind_text();
+                        //const char   *axID = coord_info::axis_name(dim);
 
                         if(g.kind!=ghosts::async)
                         {
-                            MPI.Printf(stderr,"-%s.%s\n",kind,axID);
+                            //MPI.Printf(stderr,"-%s.%s\n",kind,axID);
                             continue;
                         }
 
                         ghosts_io    &IO    = GhostsIO[dim]; assert(IO.capacity>=g.size()*sizeof(T));
-                        MPI.Printf(stderr,"+%s.%s\n",kind,axID);
+                        //MPI.Printf(stderr,"+%s.%s\n",kind,axID);
 
                         switch(g.flag)
                         {
+                                //______________________________________________
+                                //
                                 // symetric case
+                                //______________________________________________
                             case ghosts::has_both: {
                                 const ghosts_pair *sendID = 0;
                                 const ghosts_pair *recvID = 0;
@@ -155,7 +137,7 @@ namespace yocto
                                         recvID = g.prev;
                                         break;
                                 }
-                                MPI.Printf(stderr,"+%s.%s: send->%ld,recv<-%ld\n",kind,axID,sendID->rank,recvID->rank);
+                                //MPI.Printf(stderr,"+%s.%s: send->%ld,recv<-%ld\n",kind,axID,sendID->rank,recvID->rank);
                                 const size_t nxch = IO.load_into_send(*sendID,F);
                                 MPI.Sendrecv(IO.send_addr(), nxch, MPI_BYTE, sendID->rank, 0,
                                              IO.recv_addr(), nxch, MPI_BYTE, recvID->rank, 0,
@@ -163,7 +145,10 @@ namespace yocto
                                 IO.save_from_recv(*recvID,F);
                             } break;
 
+                                //______________________________________________
+                                //
                                 // prev only
+                                //______________________________________________
                             case ghosts::has_prev:
                             {
                                 switch(pass)
@@ -177,7 +162,10 @@ namespace yocto
                                 }
                             } break;
 
-                                //next only
+                                //______________________________________________
+                                //
+                                // next only
+                                //______________________________________________
                             case ghosts::has_next:
                             {
                                 switch(pass)
@@ -198,77 +186,7 @@ namespace yocto
                     }
                 }
 
-#if 0
-                for(coord1d pass=0;pass<2;++pass)
-                {
-                    MPI.Printf0(stderr,"--\tPass #%d\n",int(pass));
 
-                    for(size_t dim=0;dim<DIMENSION;++dim)
-                    {
-
-                        const ghosts &g    = Ghosts[dim];
-                        const string  gk   = g.kind_text() + vformat("@%s",coord_info::axis_name(dim));
-                        const char   *kind = gk.c_str();
-
-                        if(g.kind!=ghosts::async)
-                        {
-                            MPI.Printf(stderr,"%s\n",kind);
-                            continue;
-                        }
-
-                        const int     color = __coord(F.color,dim);
-                        ghosts_io    &IO    = GhostsIO[dim]; assert(IO.capacity>=g.size()*sizeof(T));
-                        const int     sz    = F.full.size;
-                        if(color==pass)
-                        {
-                            // send
-                            switch(g.flag)
-                            {
-                                case ghosts::has_prev: {
-                                    MPI.Printf(stderr, "color=%d: send->prev=%d.%d\n",color,sz,int(g.prev->rank));
-                                    send(*g.prev, IO, F);
-                                } break;
-                                case ghosts::has_next: {
-                                    MPI.Printf(stderr, "color=%d: send->next=%d.%d\n",color,sz,int(g.next->rank));
-                                    send(*g.next,IO,F);
-                                } break;
-                                case ghosts::has_both: {
-                                    MPI.Printf(stderr, "color=%d: send->prev=%d.%d and send->next=%d.%d\n",color,sz,int(g.prev->rank),sz,int(g.next->rank));
-                                    send(*g.prev,IO,F);
-                                    send(*g.next,IO,F);
-                                } break;
-                                default:
-                                    throw exception("mpi_exchange: invalid ghosts flag=%d", g.flag);
-                            }
-                        }
-                        else
-                        {
-                            // recv mode
-                            switch(g.flag)
-                            {
-                                case ghosts::has_prev: {
-                                    MPI.Printf(stderr, "color=%d: recv<-prev=%d.%d\n",color,sz,int(g.prev->rank));
-                                    recv(*g.prev,IO,F);
-                                } break;
-                                case ghosts::has_next: {
-                                    MPI.Printf(stderr, "color=%d: recv<-next=%d.%d\n",color,sz,int(g.next->rank));
-                                    recv(*g.next,IO,F);
-                                } break;
-                                case ghosts::has_both: {
-                                    MPI.Printf(stderr, "color=%d: recv<-prev=%d.%d and recv<-next=%d.%d\n",color,sz,int(g.prev->rank),sz,int(g.next->rank));
-                                    recv(*g.prev,IO,F);
-                                    recv(*g.next,IO,F);
-                                } break;
-                                default:
-                                    throw exception("mpi_exchange: invalid ghosts flag=%d", g.flag);
-                            }
-                            
-                        }
-                    }
-                    
-                }
-#endif
-                
             }
             
             
