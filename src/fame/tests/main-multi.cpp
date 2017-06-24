c2ee9912c208c11c7e8e735a9f2685ad9f78ede0
ybouret <yann.bouret@gmail.com>
2017-06-24 19:34:47 +0200

    test multi

--- src/fame/tests/main-multi.cpp
+++ src/fame/tests/main-multi.cpp
@@ -6,6 +6,18 @@ using namespace fame;
 
 YOCTO_PROGRAM_START()
 {
+    YOCTO_MPI_ENV();
+    const coord1d ng = 2;
+    
+    {
+        const layout<coord1d> full(1,32);
+        const domain<coord1d> D(MPI.CommWorldRank,MPI.CommWorldSize,NULL,full,1);
+        //layouts<coord1d>      W(D,ng);
+        
+        field1d<double>::pointer F1( new field1d<double>("F1",D,ng) );
+        ghosts_of<coord1d>       G(*F1);
+
+    }
     
 }
 YOCTO_PROGRAM_END()
