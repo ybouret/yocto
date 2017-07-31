2a248a690e057309bf046c75afff3bac34c88a20
ybouret <yann.bouret@gmail.com>
2017-07-31 13:32:34 +0200

    detecting posix group...

--- src/lang/yocto/lang/pattern/regexp.cpp
+++ src/lang/yocto/lang/pattern/regexp.cpp
@@ -54,6 +54,8 @@ namespace yocto
 #define Y_ALTERN '|'
 #define Y_LBRACE '{'
 #define Y_RBRACE '}'
+#define Y_LBRACK '['
+#define Y_RBRACK ']'
 
             inline Pattern *subExpr()
             {
@@ -127,6 +129,14 @@ namespace yocto
                             //__________________________________________________
                         case Y_LBRACE:
                             createBracedJoker(sxp.operands);
+                            break;
+
+                            //__________________________________________________
+                            //
+                            // start a new group
+                            //__________________________________________________
+                        case Y_LBRACK:
+                            createGroup(sxp.operands);
                             break;
 
                             //__________________________________________________
@@ -272,6 +282,77 @@ namespace yocto
                     // assuming counting joker
                 }
 
+            }
+
+
+            //==================================================================
+            //
+            //
+            // create a group and add it to operands
+            //
+            //
+            //==================================================================
+            void createGroup( Patterns &ops )
+            {
+                assert(Y_LBRACK==*curr);
+                if(++curr>last) throw exception("%sunfinished group",fn);
+
+
+                const char C = curr[0];
+                switch(C)
+                {
+                    case ':': ops.push_back(findPosixExpression()); return;
+                    default:
+                        throw exception("group not handled");
+                }
+            }
+
+            //==================================================================
+            //
+            // extract a posix sub-expression
+            //
+            //==================================================================
+            Pattern * findPosixExpression()
+            {
+                assert(':'==*curr);
+                const char *ini = ++curr; // skip ':'
+                while(true)
+                {
+                    if(curr>=last) throw exception("%sunfinished posix expression",fn);
+                    if(curr[0]==':')
+                    {
+                        if(curr[1]!=Y_RBRACK) throw exception("%sexpecting ':%c'",fn,Y_RBRACK);
+                        break;
+                    }
+                    ++curr;
+                }
+                assert(':'      == curr[0]);
+                assert(Y_RBRACK == curr[1]);
+                const char *end = curr;
+                ++curr;
+                ++curr;
+
+                const size_t n = static_cast<size_t>(end-ini);
+                if(n<=0) throw exception("%sempty posix expression",fn);
+
+                const string id(ini,n);
+#define Y_LANG_POSIX(NAME) if(#NAME==id) return posix::NAME()
+
+                Y_LANG_POSIX(upper);
+                Y_LANG_POSIX(lower);
+                Y_LANG_POSIX(digit);
+                Y_LANG_POSIX(alpha);
+                Y_LANG_POSIX(alnum);
+                Y_LANG_POSIX(xdigit);
+                Y_LANG_POSIX(blank);
+                Y_LANG_POSIX(space);
+                Y_LANG_POSIX(punct);
+
+                Y_LANG_POSIX(word);
+                Y_LANG_POSIX(endl);
+                Y_LANG_POSIX(dot);
+
+                throw exception("%sunknown posix :%s:",fn,id.c_str());
             }
 
         private:
