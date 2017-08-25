da64624463826ca50a7a21fb759d313c61952525
ybouret <yann.bouret@gmail.com>
2017-08-25 10:23:47 +0200

    need to create a newLine for translator...

--- src/lang/tests/test-trans.cpp
+++ src/lang/tests/test-trans.cpp
@@ -10,7 +10,15 @@ class myTrans : public Lexical::Translator
 {
 public:
 
-    explicit myTrans() : Lexical::Translator("lexer","main")
+    Lexical::Scanner &com1;
+    Lexical::Scanner &com2;
+    Token             data;
+
+    explicit myTrans() :
+    Lexical::Translator("lexer","main"),
+    com1( declare("com1") ),
+    com2( declare("com2") ),
+    data()
     {
 
         root.make("int",   "[0-9]+",     this, &myTrans::emit);
@@ -19,9 +27,12 @@ public:
         root.make("blanks","[:blank:]+", this, &myTrans::drop);
         root.make("endl",  "[:endl:]",   this, &myTrans::endl);
 
-        Lexical::Scanner &com1 = declare("com1");
         com1.back("[:endl:]",  this, &myTrans::leaveCom1);
         com1.make("other",".", this, &myTrans::dropCom);
+
+        root.call("com2","/\\*",this, &myTrans::enterCom2);
+
+        com2.back("\\*/",this,&myTrans::leaveCom2);
 
     }
 
@@ -31,73 +42,76 @@ public:
 
     Lexical::Result emit(const Token &tkn) throw()
     {
-        std::cerr << "EMIT '" << tkn << "'" << std::endl;
         return Lexical::Forward;
-        }
+    }
 
-        Lexical::Result drop(const Token &tkn) throw()
+    Lexical::Result drop(const Token &tkn) throw()
+    {
+        return Lexical::Discard;
+    }
+
+    Lexical::Result dropCom(const Token &tkn) throw()
+    {
+        return Lexical::Discard;
+    }
+
+    Lexical::Result endl(const Token &) throw()
+    {
+        root.module->newLine();
+        return Lexical::Discard;
+    }
+
+    void enterCom1(const Token &)
+    {
+    }
+
+    void leaveCom1(const Token &)
+    {
+        com1.module->newLine();
+    }
+
+    void enterCom2(const Token &)
+    {
+        data.clear();
+    }
+
+    void leaveCom2(const Token&)
+    {
+
+    }
+
+private:
+    YOCTO_DISABLE_COPY_AND_ASSIGN(myTrans);
+};
+
+
+YOCTO_UNIT_TEST_IMPL(trans)
+{
+    myTrans trans;
+    Module::Handle hModule( new Module() ); // open stdio
+    Source         source( hModule );       // register it in source
+
+
+    Lexical::Units lexemes;
+
+    for(;;)
+    {
+        Lexical::Unit *u = trans.run(source);
+        if(u) lexemes.push_back(u); else break;
+    }
+
+    for(const Lexical::Unit *u = lexemes.head; u; u=u->next )
+    {
+        std::cerr << u->label;
+        for(size_t i=u->label.length();i<=trans.root.max_label_length;++i)
         {
-            std::cerr << "DROP '" << tkn << "'" << std::endl;
-            return Lexical::Discard;
+            std::cerr << ' ';
         }
-
-        Lexical::Result dropCom(const Token &tkn) throw()
-        {
-            std::cerr << "DROPCOM '" << tkn << "'" << std::endl;
-            return Lexical::Discard;
-        }
-
-        Lexical::Result endl(const Token &) throw()
-        {
-            std::cerr << "NEWLINE" << std::endl;
-            root.module->newLine();
-            return Lexical::Discard;
-        }
-
-        void enterCom1(const Token &)
-        {
-            std::cerr << "<COM1>" << std::endl;
-        }
-
-        void leaveCom1(const Token &)
-        {
-            std::cerr << "</COM1>" << std::endl;
-        }
+        std::cerr << ": ";
+        std::cerr << "'" << *u  << "'";
+        std::cerr << std::endl;
+    }
 
 
-
-    private:
-        YOCTO_DISABLE_COPY_AND_ASSIGN(myTrans);
-        };
-
-
-        YOCTO_UNIT_TEST_IMPL(trans)
-        {
-            myTrans trans;
-            Module::Handle hModule( new Module() ); // open stdio
-            Source         source( hModule );       // register it in source
-
-
-            Lexical::Units lexemes;
-
-            for(;;)
-            {
-                Lexical::Unit *u = trans.run(source);
-                if(u) lexemes.push_back(u); else break;
-            }
-
-            for(const Lexical::Unit *u = lexemes.head; u; u=u->next )
-            {
-                std::cerr << u->label;
-                for(size_t i=u->label.length();i<=trans.root.max_label_length;++i)
-                {
-                    std::cerr << ' ';
-                }
-                std::cerr << ": ";
-                std::cerr << "'" << *u  << "'";
-                std::cerr << std::endl;
-            }
-            
-            
-        }
-        YOCTO_UNIT_TEST_DONE()
+}
+YOCTO_UNIT_TEST_DONE()
