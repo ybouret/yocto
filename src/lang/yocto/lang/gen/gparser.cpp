ee344d685b94821e5276a3027b53d91ecd332bcf
ybouret <yann.bouret@gmail.com>
2017-09-01 11:23:09 +0200

    init gParser

---
+++ src/lang/yocto/lang/gen/gparser.cpp
@@ -0,0 +1,24 @@
+#include "yocto/lang/gen/gparser.hpp"
+
+namespace yocto
+{
+    namespace Lang
+    {
+        namespace Syntax
+        {
+
+            gParser:: ~gParser() throw()
+            {
+            }
+
+
+            gParser:: gParser() :
+            Parser("gParser")
+            {
+            }
+            
+        }
+
+    }
+}
+
---
+++ src/lang/yocto/lang/gen/gparser.hpp
@@ -0,0 +1,27 @@
+#ifndef YOCTO_LANG_GEN_GPARSER_INCLUDED
+#define YOCTO_LANG_GEN_GPARSER_INCLUDED 1
+
+#include "yocto/lang/syntax/parser.hpp"
+
+namespace yocto
+{
+    namespace Lang
+    {
+        namespace Syntax
+        {
+
+            class gParser : public Parser
+            {
+            public:
+                virtual ~gParser() throw();
+                explicit gParser();
+
+            private:
+                YOCTO_DISABLE_COPY_AND_ASSIGN(gParser);
+
+            };
+        }
+    }
+}
+
+#endif
