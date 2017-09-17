a31cfdbc861c4b9ad0344d5c575296f133559e8c
ybouret <yann.bouret@gmail.com>
2017-09-17 10:48:35 +0200

    WIP dynamo

--- src/lang/yocto-dynamo.cpp
+++ src/lang/yocto-dynamo.cpp
@@ -1,15 +1,30 @@
 #include "yocto/program.hpp"
 #include "yocto/lang/syntax/parser.hpp"
+#include "yocto/ptr/auto.hpp"
+#include "yocto/fs/vfs.hpp"
+#include "yocto/ios/ocstream.hpp"
 
 using namespace yocto;
 using namespace Lang;
 
 YOCTO_PROGRAM_START()
 {
-    if(argc<=1)
+    if(argc<=2)
     {
-        throw exception("usage: %s grammar.g",program);
+        throw exception("usage: %s grammar.g dir",program);
     }
-    
+    const string             gramFile = argv[1];
+    const string             gramWork = argv[2];
+    vfs::as_directory((string&)gramWork);
+    const string             inclExt  = ".inc";
+
+    auto_ptr<Syntax::Parser> parser( Syntax::Parser::Generate(gramFile) );
+    const string inclFile = gramWork + parser->tag + inclExt;
+    std::cerr << "Saving into " << inclFile << std::endl;
+    {
+        ios::wcstream fp(inclFile);
+        Syntax::Parser::Encode(gramFile,fp);
+    }
+
 }
 YOCTO_PROGRAM_END()
