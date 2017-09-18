ee29812d0eb41e6e6986a5d2f8991231afd05162
ybouret <yann.bouret@gmail.com>
2017-09-18 09:00:53 +0200

    WIP Json loader

---
+++ src/json/yocto/json/loader.cpp
@@ -0,0 +1,11 @@
+#include "yocto/json/loader.hpp"
+
+#include "json.def"
+
+namespace yocto
+{
+    namespace JSON
+    {
+
+    }
+}
---
+++ src/json/yocto/json/loader.hpp
@@ -0,0 +1,14 @@
+#ifndef YOCTO_JSON_LOADER_INCLUDED
+#define YOCTO_JSON_LOADER_INCLUDED 1
+
+#include "yocto/json/value.hpp"
+
+namespace yocto
+{
+    namespace JSON
+    {
+        
+    }
+}
+
+#endif
