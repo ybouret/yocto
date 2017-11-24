895b1785540f6da30eb970df2d7692b31f29f85c
ybouret <yann.bouret@gmail.com>
2017-11-24 13:05:09 +0100

    ready for __rand32, need to simplify the full method

--- src/yocto/yocto/randomized/urand32.hpp
+++ src/yocto/yocto/randomized/urand32.hpp
@@ -7,7 +7,57 @@ namespace yocto
 {
     namespace Randomized
     {
-        
+        //! Marsaglia's 32 bit random generators
+        class __rand32
+        {
+        public:
+            uint32_t z,w,jsr,jcong,a,b;
+            uint32_t x,y,bro;
+            uint8_t  c;
+            uint32_t t[256];
+            
+            explicit __rand32() throw();
+            virtual ~__rand32() throw();
+            
+            //typedef uint32_t (rand32::*generator)();
+            uint32_t mwc()   throw();
+            uint32_t shr3()  throw();
+            uint32_t cong()  throw();
+            uint32_t fib()   throw();
+            uint32_t kiss()  throw();
+            uint32_t lfib4() throw();
+            uint32_t swb()   throw();
+            
+            void settable( uint32_t i1, uint32_t i2, uint32_t i3, uint32_t i4, uint32_t i5, uint32_t i6 ) throw();
+            void reset( uint32_t s ) throw();
+            
+            //! make a random 0 or 1
+            template <typename T>
+            static inline T to_bit( const uint32_t u ) throw()
+            {
+                static const uint32_t threshold = uint32_t(1) << 31;
+                static const T __zero(0);
+                static const T __one(1);
+                return u >= threshold ? __one : __zero;
+            }
+            
+            
+            //! convert to 0:1 exclusive
+            static inline double to_double( const uint32_t u ) throw()
+            {
+                return (0.5+double(u))/4294967296.0;
+            }
+            
+            //! convert to 0:1 exclusive
+            static inline float to_float( const uint32_t u ) throw()
+            {
+                return (0.5f+float(u))/4294967296.0f;
+            }
+            
+            static void test();
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(__rand32);
+        };
     }
 }
 
