6a12382f0bea0cb535aaf88e7e8ae8882f97e911
ybouret <yann.bouret@gmail.com>
2017-11-29 08:50:49 +0100

    need to compute expected number...

--- src/yocto/tests-mpl/test-rand.cpp
+++ src/yocto/tests-mpl/test-rand.cpp
@@ -1,5 +1,5 @@
 #include "yocto/utest/run.hpp"
-#include "yocto/mpl/natural.hpp"
+#include "yocto/mpl/rational.hpp"
 
 using namespace yocto;
 
@@ -12,6 +12,25 @@ YOCTO_UNIT_TEST_IMPL(rand)
         {
             mpn m = mpn::rand(bits);
             std::cerr << std::hex << "\tm=" << m << ", bits=" << std::dec << m.bits() << std::endl;
+
+
+        }
+
+        mpn ave = 0;
+        mpn n   = 0;
+        for(size_t i=10000;i>0;--i)
+        {
+            ave += mpn::rand(bits);
+            ++n;
+        }
+        ave /= n;
+        std::cerr << std::hex << "ave = " << ave << std::endl;
+        if(bits>0)
+        {
+            const mpn expected = mpn::exp2(bits-1);
+            std::cerr << "for = " << expected << std::endl;
+            mpq ratio(ave,expected);
+            std::cerr << "ratio=" << ratio.to_double() << std::endl;
         }
     }
 }
