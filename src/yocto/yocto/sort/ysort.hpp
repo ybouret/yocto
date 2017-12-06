c4c1a1a441f25e7f4e8bed42d3799eb1b18bb040
ybouret <yann.bouret@gmail.com>
2017-12-06 08:46:15 +0100

    will generate sorting networks...

--- src/yocto/yocto/sort/ysort.hpp
+++ src/yocto/yocto/sort/ysort.hpp
@@ -7,42 +7,6 @@
 
 namespace yocto
 {
-    inline void __echanger(int tableau[], int a, int b)
-    {
-        int temp = tableau[a];
-        tableau[a] = tableau[b];
-        tableau[b] = temp;
-    }
-
-    inline void __ySort(int tableau[], int debut, int fin)
-    {
-        int gauche = debut-1;
-        int droite = fin+1;
-        const int pivot = tableau[debut];
-
-        /* Si le tableau est de longueur nulle, il n'y a rien à faire. */
-        if(debut >= fin)
-            return;
-
-        /* Sinon, on parcourt le tableau, une fois de droite à gauche, et une
-         autre de gauche à droite, à la recherche d'éléments mal placés,
-         que l'on permute. Si les deux parcours se croisent, on arrête. */
-        while(1)
-        {
-            do droite--; while(tableau[droite] > pivot);
-            do gauche++; while(tableau[gauche] < pivot);
-
-            if(gauche < droite)
-                __echanger(tableau, gauche, droite);
-            else break;
-        }
-
-        /* Maintenant, tous les éléments inférieurs au pivot sont avant ceux
-         supérieurs au pivot. On a donc deux groupes de cases à trier. On utilise
-         pour cela... la méthode quickSort elle-même ! */
-        __ySort(tableau, debut, droite);
-        __ySort(tableau, droite+1, fin);
-    }
 
     template <typename T,typename FUNC>
     inline void _ySort(T        *tableau,
