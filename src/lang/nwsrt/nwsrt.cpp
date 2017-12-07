56725c8601e867c34dc3fddc7f7af7daf1765113
ybouret <yann.bouret@gmail.com>
2017-12-07 17:33:40 +0100

    WIP nwsrt

--- src/lang/nwsrt/nwsrt.cpp
+++ src/lang/nwsrt/nwsrt.cpp
@@ -6,14 +6,15 @@
 using namespace yocto;
 using namespace Lang;
 
-static const char nwsrt_grammar[] = {
+static const char nwsrt_grammar[] =
+{
 #include "nwsrt.inc"
 };
 
 class TheCompiler : public Compiler
 {
     public:
-
+    
     explicit TheCompiler() : Compiler(Syntax::Parser::Generate(nwsrt_grammar,sizeof(nwsrt_grammar)))
     {
     }
@@ -22,16 +23,37 @@ class TheCompiler : public Compiler
     {
     }
 
+    virtual void onTerminal(const string &label,
+                            const int     hCode,
+                            const string &content)
+    {
+        __indent() << "push "; __output(label) << " '" << content << "'" << std::endl;
+
+    }
+
+    virtual void onInternal(const string &label,
+                            const int     hCode,
+                            const int     nArgs)
+    {
+        __indent() << "call " << (label) << "/" << nArgs << std::endl;
+    }
+
+    private:
+    YOCTO_DISABLE_COPY_AND_ASSIGN(TheCompiler);
+
 };
 
 
 YOCTO_PROGRAM_START()
 {
     TheCompiler nws;
-    const Module::Handle hModule( new Module() );
-    Source               source( hModule );
+    if(argc>1)
+    {
+        const Module::Handle hModule( new Module(argv[1]) );
+        Source               source( hModule );
 
-    nws.ld(source);
+        nws.ld(source);
+    }
 
 }
 YOCTO_PROGRAM_END()
