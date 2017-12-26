a069e41e6b489ecf38930f1638036d5d14463fe5
ybouret <yann.bouret@gmail.com>
2017-12-26 10:27:24 +0100

    new test code for isqrt

--- src/yocto/tests/test-isqrt.cpp
+++ src/yocto/tests/test-isqrt.cpp
@@ -15,15 +15,22 @@ YOCTO_UNIT_TEST_IMPL(isqrt)
     
     
     std::cerr.flush();
+    static const uint64_t K = 100;
+    uint64_t old_k = 0;
     for(uint64_t i=0;i<=uint64_t(n);++i)
     {
         const uint64_t ans = iSquareRoot(i);
-        fprintf(stderr,"isqrt(%u)=%u   \r", unsigned(i), unsigned(ans) );
-        fflush(stderr);
-        //fprintf(stderr,"\n");
-        if(ans*ans>i)         throw exception("too high!");
-       // if((ans+1)*(ans+1)<i) throw exception("too low!");
+        const uint64_t k   = (K*i)/n;
+        if( (n==i) || (k-old_k>=1) )
+        {
+            fprintf(stderr,"isqrt(%u)=%u   \r", unsigned(i), unsigned(ans) );
+            fflush(stderr);
+            old_k = k;
+        }
+        if(ans*ans>i)         throw exception("%lu: too high!", (unsigned long)i);
+        if((ans+1)*(ans+1)<i) throw exception("%lu: too low!",  (unsigned long)i);
     }
     fprintf(stderr,"\n\n");
 }
 YOCTO_UNIT_TEST_DONE()
+
