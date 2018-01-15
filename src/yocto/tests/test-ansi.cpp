3f2a095e751d41be2b94181ddeb5dd143592e75f
ybouret <yann.bouret@gmail.com>
2018-01-15 14:31:47 +0100

    typo

--- src/yocto/tests/test-ansi.cpp
+++ src/yocto/tests/test-ansi.cpp
@@ -30,6 +30,7 @@ YOCTO_UNIT_TEST_IMPL(null_device_cpp)
     {
         throw exception("ifstream(%s)",YOCTO_NULL_DEVICE);
     }
+    
 
     std::ofstream f_out(YOCTO_NULL_DEVICE);
     if( f_out.fail() )
