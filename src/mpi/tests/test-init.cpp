9bc579ebcb231fca78b90d22fcea568c8f6ad9b5
ybouret <yann.bouret@gmail.com>
2018-01-16 09:08:01 +0100

    checking singletons...

--- src/mpi/tests/test-init.cpp
+++ src/mpi/tests/test-init.cpp
@@ -2,12 +2,15 @@
 
 #include "yocto/mpi/mpi.hpp"
 #include "yocto/associative/type-glossary.hpp"
+#include "yocto/ios/null-device.hpp"
 
 using namespace yocto;
 
 YOCTO_UNIT_TEST_IMPL(init)
 {
-    
+    singleton_verbosity = true;
+    (void)ios::null_device::instance();
+
     YOCTO_MPI_ENV();
     //MPI.CloseStdIO();
     MPI.Printf(stderr, "'%s' is ready\n",MPI.ProcessorName);
@@ -15,7 +18,7 @@ YOCTO_UNIT_TEST_IMPL(init)
 
     type_glossary<MPI_Datatype> tg;
     tg.enroll<char>(MPI_CHAR);
-    //tg.enroll<int8_t>(MPI_INT8_T);
+    tg.enroll<int8_t>(MPI_INT8_T);
     
 
 }
