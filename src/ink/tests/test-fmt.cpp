588abd50a0b3e502400c920c7d7a8f33e790227c
ybouret <yann.bouret@gmail.com>
2018-01-28 14:11:26 +0100

    will remove images

--- src/ink/tests/test-fmt.cpp
+++ src/ink/tests/test-fmt.cpp
@@ -1,9 +1,5 @@
 
-#include "yocto/ink/pixmaps.hpp"
-#include "yocto/ink/image/png.hpp"
-#include "yocto/ink/image/jpeg.hpp"
-#include "yocto/ink/image/tiff.hpp"
-
+#include "yocto/ink/images.hpp"
 #include "yocto/utest/run.hpp"
 
 using namespace yocto;
@@ -15,9 +11,12 @@ YOCTO_UNIT_TEST_IMPL(fmt)
     Ink::png_format  &PNG = IMG.create<Ink::png_format>();
     Ink::jpeg_format &JPG = IMG.create<Ink::jpeg_format>();
     Ink::tiff_format &TIF = IMG.create<Ink::tiff_format>();
-    std::cerr << PNG.name << std::endl;
-    std::cerr << JPG.name << std::endl;
-    std::cerr << TIF.name << std::endl;
+    (void)PNG;
+    (void)JPG;
+    (void)TIF;
+   // std::cerr << PNG.name << std::endl;
+   // std::cerr << JPG.name << std::endl;
+   // std::cerr << TIF.name << std::endl;
 
 }
 YOCTO_UNIT_TEST_DONE()
