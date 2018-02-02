f19f64cefda1c6f9ccb198c82996cd952a7b98e8
ybouret <yann.bouret@gmail.com>
2018-02-02 10:47:00 +0100

    WIP divide

--- src/yocto/yocto/ipso/divide.cpp
+++ src/yocto/yocto/ipso/divide.cpp
@@ -1,12 +1,5 @@
 #include "yocto/ipso/divide.hpp"
 
-#define Y_IPSO_SHOW 1
-#if defined(Y_IPSO_SHOW) && (1==Y_IPSO_SHOW)
-#include <cstdio>
-#define Y_IPSO_CODE(CODE) CODE
-#else
-#define Y_IPSO_CODE(CODE)
-#endif
 
 namespace yocto
 {
