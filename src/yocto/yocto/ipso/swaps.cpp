c3e108b00737b771cc2cd5fcd0eb2a10104df5fb
ybouret <yann.bouret@gmail.com>
2018-02-20 10:44:09 +0100

    start swaps...

--- src/yocto/yocto/ipso/swaps.cpp
+++ src/yocto/yocto/ipso/swaps.cpp
@@ -4,6 +4,26 @@ namespace yocto
 {
     namespace ipso
     {
+
+
+        swaps:: ~swaps() throw()
+        {
+        }
+
+        swaps:: swaps(const size_t source_rank,
+                      const size_t target_rank,
+                      const size_t some_layers) throw() :
+        send(),
+        recv(),
+        count(0),
+        source(source_rank),
+        target(target_rank),
+        layers(some_layers),
+        next(0),
+        prev(0)
+        {
+            assert(layers>0);
+        }
         
     }
 }
--- src/yocto/yocto/ipso/swaps.hpp
+++ src/yocto/yocto/ipso/swaps.hpp
@@ -9,7 +9,32 @@ namespace yocto
 {
     namespace ipso
     {
+        //! a swap is a list of offsets
         typedef vector<coord1D> swap;
+
+        //! MPI style swaps, local or async
+        class swaps : public object
+        {
+        public:
+            typedef core::list_of_cpp<swaps> list;
+            
+            const swap    send;   //!< inner indices
+            const swap    recv;   //!< outer indices
+            const size_t  count;  //!< send.size() = recv.size() = count
+            const size_t  source; //!< rank of source
+            const size_t  target; //!< rank of target
+            const coord1D layers; //!< number of extra layers
+            swaps        *next;
+            swaps        *prev;
+
+            explicit swaps(const size_t source_rank,
+                           const size_t target_rank,
+                           const size_t some_layers) throw();
+            virtual ~swaps() throw();
+
+        private:
+            YOCTO_DISABLE_COPY_AND_ASSIGN(swaps);
+        };
 
 
     }
