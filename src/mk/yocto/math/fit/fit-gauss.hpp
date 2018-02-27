6dee9f09efef784ebe32fa3168c0bf6c034ecff6
ybouret <yann.bouret@gmail.com>
2018-02-27 08:27:55 +0100

    will do fit gauss...

---
+++ src/mk/yocto/math/fit/fit-gauss.hpp
@@ -0,0 +1,18 @@
+
+#ifndef YOCTO_MATH_FIT_GAUSS_INCLUDED
+#define YOCTO_MATH_FIT_GAUSS_INCLUDED 1
+
+#include "yocto/math/fit/fit.hpp"
+
+namespace yocto
+{
+    namespace math
+    {
+        
+        
+        
+    }
+}
+
+#endif
+
