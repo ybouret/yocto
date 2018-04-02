22b16ada5b2295b4b6e28a06bd068542eac8c41a
ybouret <yann.bouret@gmail.com>
2018-04-02 16:20:21 +0200

    need to make an automatic sizes selector function...

--- src/visit/tests/test-ipso.cpp
+++ src/visit/tests/test-ipso.cpp
@@ -1,11 +1,96 @@
 #include "yocto/ipso/visit.hpp"
 #include "yocto/utest/run.hpp"
+#include "yocto/string/conv.hpp"
 
 using namespace yocto;
+using namespace ipso;
+
+namespace
+{
+
+    class Sim : public VisIt::Simulation
+    {
+    public:
+        const size_t   size;
+        const size_t   rank;
+
+        coord1D        fallback1D;
+        const patch1D  region1D;
+        const coord1D  pbcs1D;
+        const coord1D  sizes1D;
+        const divide::in1D full1D;
+
+        explicit Sim(const VisIt   &visit,
+                     const coord3D &dims,
+                     const coord3D &pbcs,
+                     const size_t   layers) :
+        VisIt::Simulation(visit),
+        size( MPI.CommWorldSize ),
+        rank( MPI.CommWorldRank ),
+        fallback1D(0),
+        region1D(1,dims.x),
+        pbcs1D(pbcs.x),
+        sizes1D( check_sizes(mapping<coord1D>::optimal_sizes_for(size,region1D,layers,pbcs1D,&fallback1D),fallback1D,size) ),
+        full1D(sizes1D,region1D)
+        {
+
+
+        }
+
+        virtual ~Sim() throw()
+        {
+        }
+
+
+    private:
+        YOCTO_DISABLE_COPY_AND_ASSIGN(Sim);
+        template <typename COORD>
+        COORD check_sizes( const COORD sz, const COORD &fb, const coord1D np )
+        {
+            COORD ans = sz;
+            if( __coord_prod(ans) < np )
+            {
+                ans = fb;
+                if(__coord_prod(ans) < np )
+                {
+                    throw exception("too many cpus for given domain size");
+                }
+            }
+            return ans;
+        }
+
+    };
+
+}
 
 YOCTO_UNIT_TEST_IMPL(ipso)
 {
-    
+    YOCTO_MPI_ENV();
+    const string sim_name = "ipso";
+    const string sim_comm = "testing ipso API";
+    //const string sim_gui  = "loop.ui";
+
+    // prepare VisIt
+    VisIt &visit = VisIt::Start(sim_name,
+                                sim_comm,
+                                MPI,
+                                NULL,
+                                false);
+
+
+    // prepare args
+    if(argc<=3)
+    {
+        throw exception("usage: %s DIMS PBCS LAYERS", argv[0]);
+    }
+    const coord3D dims   = __coord_parser::get<coord3D>(argv[1],"dims");
+    const coord3D pbcs   = __coord_parser::get<coord3D>(argv[2],"pbcs");
+    const size_t  layers = strconv::to_size(argv[3],"layers");
+
+    Sim sim(visit,dims,pbcs,layers);
+
+    sim.loop();
+
 }
 YOCTO_UNIT_TEST_DONE()
 
--- src/yocto/yocto/ipso/visit.hpp
+++ src/yocto/yocto/ipso/visit.hpp
@@ -5,26 +5,53 @@
 #include "yocto/ipso/mesh/rectilinear.hpp"
 #include "yocto/ipso/mesh/curvilinear.hpp"
 #include "yocto/ipso/mesh/point.hpp"
+#include "yocto/ipso/mapping.hpp"
 
 namespace yocto
 {
     namespace ipso
     {
 
-        struct visit
+        struct __visit
         {
             ////////////////////////////////////////////////////////////////////
             //
-            // mesh API
+            // Mesh Meta Data API
             //
             ////////////////////////////////////////////////////////////////////
-            template <typename T,const size_t DIM>
-            void add_mesh_metadata(visit_handle &md, const rectilinear_mesh<T,DIM> &mesh)
+            static inline
+            void __add_mesh(visit_handle    &m,
+                            const mesh_info &mesh)
+            {
+                YOCTO_MPI_GET();
+                const size_t d = mesh.dimension;
+                VisIt_MeshMetaData_setName(m,*mesh.name);
+                VisIt_MeshMetaData_setSpatialDimension(m,d);
+                VisIt_MeshMetaData_setNumDomains(m,MPI.CommWorldSize);
+                switch(d)
+                {
+                    case 3: VisIt_MeshMetaData_setZLabel(m,*mesh[2].name);
+                    case 2: VisIt_MeshMetaData_setYLabel(m,*mesh[1].name);
+                    case 1: VisIt_MeshMetaData_setXLabel(m,*mesh[0].name);
+                    default:
+                        break;
+                }
+
+            }
+
+            template <typename MESH> static inline
+            void __add_mesh_metadata(visit_handle &md,
+                                     const MESH   &mesh,
+                                     const int     mesh_type,
+                                     const int     topo_dims)
             {
                 visit_handle m = VisIt::MeshMetaData_alloc();
                 try
                 {
-                    VisIt_MeshMetaData_setName(mesh,*mesh.name());
+                    __add_mesh(m,mesh);
+                    VisIt_MeshMetaData_setMeshType(m,mesh_type);
+                    VisIt_MeshMetaData_setTopologicalDimension(m,topo_dims);
+                    VisIt_SimulationMetaData_addMesh(md,m);
                 }
                 catch(...)
                 {
@@ -32,6 +59,25 @@ namespace yocto
                     throw;
                 }
             }
+
+            template <typename T,const size_t DIM> static inline
+            void add_mesh_metadata(visit_handle &md, const rectilinear_mesh<T,DIM> &mesh)
+            {
+                __add_mesh_metadata(md,mesh,VISIT_MESHTYPE_RECTILINEAR,mesh.dimension);
+            }
+
+            template <typename T,const size_t DIM> static inline
+            void add_mesh_metadata(visit_handle &md, const curvilinear_mesh<T,DIM> &mesh)
+            {
+                __add_mesh_metadata(md,mesh,VISIT_MESHTYPE_CURVILINEAR,mesh.dimension);
+            }
+
+            template <typename T,const size_t DIM> static inline
+            void add_mesh_metadata(visit_handle &md, const point_mesh<T,DIM> &mesh)
+            {
+                __add_mesh_metadata(md,mesh,VISIT_MESHTYPE_POINT,1);
+            }
+
 
         };
 
