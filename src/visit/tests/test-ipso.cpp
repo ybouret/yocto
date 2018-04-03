cf3346b8556555b23e4d8e729bc34a0008449621
ybouret <yann.bouret@gmail.com>
2018-04-03 16:40:20 +0200

    OK, test with phase broadcast

--- src/visit/tests/test-ipso.cpp
+++ src/visit/tests/test-ipso.cpp
@@ -2,6 +2,7 @@
 #include "yocto/utest/run.hpp"
 #include "yocto/string/conv.hpp"
 #include "yocto/ipso/mpi.hpp"
+#include "yocto/code/alea.hpp"
 
 using namespace yocto;
 using namespace ipso;
@@ -35,6 +36,7 @@ namespace
         rectilinear_mesh<float,1> rmesh1;
         curvilinear_mesh<float,1> cmesh1;
         F1D                      &f1d;
+        fields                    var1D;
 
         const patch2D             region2D;
         const coord2D             pbcs2D;
@@ -47,6 +49,7 @@ namespace
         curvilinear_mesh<float,2> cmesh2;
         F2D                      &f2d;
         F2DV                     &f2dv;
+        fields                    var2D;
 
         const patch3D             region3D;
         const coord3D             pbcs3D;
@@ -59,6 +62,7 @@ namespace
         curvilinear_mesh<float,3> cmesh3;
         F3D  &                    f3d;
         F3DV &                    f3dv;
+        fields                    var3D;
 
         explicit Sim(const VisIt   &visit,
                      const coord3D &dims,
@@ -122,7 +126,53 @@ namespace
                 rmesh2.map_regular(b2d,full2D);
                 pmesh2.map_circle(full1D);
                 cmesh2.map_regular(b2d,full2D);
+
+            }
+
+            {
+                box<float,3> b3d( v3d(0,0,0), v3d(1,1,1) );
+                rmesh3.map_regular(b3d,full3D);
+                pmesh3.map_circle(full1D);
+                cmesh3.map_regular(b3d,full3D);
+
+            }
+
+            var1D.append(f1d);
+            W1D.allocate_swaps_for(f1d);
+
+            var2D.append(f2d);
+            var2D.append(f2dv);
+            W2D.allocate_swaps_for(var2D);
+
+            var3D.append(f3d);
+            var3D.append(f3dv);
+            W3D.allocate_swaps_for(var3D);
+
+            addCommand("init", this, & Sim::init, true);
+            addCommand("sync", this, & Sim::sync, true);
+
+            init_inner();
+        }
+
+        void init_inner()
+        {
+            {
+                f1d.ld( rank+1 );
+            }
+
+            float phi_x = 6.3f * alea.get<float>();
+            float phi_y = 6.3f * alea.get<float>();
+            float scale = 1.0f + alea.get<float>();
+            MPI.Bcast(phi_x,0, MPI_COMM_WORLD);
+            MPI.Bcast(phi_y,0, MPI_COMM_WORLD);
+            MPI.Bcast(scale,0, MPI_COMM_WORLD);
+
+            {
+
+
                 f2d.ld( rank+1 );
+
+                f2dv.ldz();
                 const field1D<float> & _X = rmesh2.X();
                 const field1D<float> & _Y = rmesh2.Y();
                 for(coord1D j=W2D.inner.lower.y;j<=W2D.inner.upper.y;++j)
@@ -132,23 +182,20 @@ namespace
                     {
                         v2d &v = f2dv[j][i];
                         const float x = _X[i]-0.5f;
-                        const float fac = exp( -2.2f*(x*x+y*y) );
-                        v.x = cosf(6.0f*x)*fac;
-                        v.y = sinf(6.0f*y)*fac;
+                        const float fac = exp( -scale*(x*x+y*y) );
+                        v.x = cosf(6.0f*x+phi_x)*fac;
+                        v.y = sinf(6.0f*y+phi_y)*fac;
                     }
                 }
             }
 
             {
-                box<float,3> b3d( v3d(0,0,0), v3d(1,1,1) );
-                rmesh3.map_regular(b3d,full3D);
-                pmesh3.map_circle(full1D);
-                cmesh3.map_regular(b3d,full3D);
                 f3d.ld(rank+1);
+                f3dv.ldz();
                 const field1D<float> & _X = rmesh3.X();
                 const field1D<float> & _Y = rmesh3.Y();
                 const field1D<float> & _Z = rmesh3.Z();
-
+                
                 for(coord1D k=W3D.inner.lower.z;k<=W3D.inner.upper.z;++k)
                 {
                     const float z = _Z[k] - 0.5f;
@@ -159,18 +206,16 @@ namespace
                         {
                             v3d &v = f3dv[k][j][i];
                             const float x = _X[i]-0.5f;
-                            const float fac = exp( -2.2f*(x*x+y*y+z*z) );
-                            v.x = cosf(6.0f*x)*fac;
-                            v.y = sinf(6.0f*y)*fac;
+                            const float fac = exp( -scale*(x*x+y*y+z*z) );
+                            v.x = cosf(6.0f*x+phi_x)*fac;
+                            v.y = sinf(6.0f*y+phi_y)*fac;
                             v.z = fac;
                         }
                     }
                 }
             }
-
-            field_info::pointer fp( &f2d );
-
         }
+
 
         // adding meshes
         virtual void setMetaData(visit_handle &md)
@@ -238,10 +283,10 @@ namespace
         // getting data
         virtual visit_handle getVariable(int,const string &id)
         {
-            if(id=="f2d")  return f2d.get();
+            if(id=="f2d")  return f2d. get();
             if(id=="f2dv") return f2dv.get();
 
-            if(id=="f3d")  return f3d.get();
+            if(id=="f3d")  return f3d. get();
             if(id=="f3dv") return f3dv.get();
 
             return VISIT_INVALID_HANDLE;
@@ -251,7 +296,20 @@ namespace
         {
         }
 
+        void init(YOCTO_VISIT_SIMULATION_CALLBACK_PROTO)
+        {
+            init_inner();
+            update();
+        }
 
+        void sync(YOCTO_VISIT_SIMULATION_CALLBACK_PROTO)
+        {
+            W1D.synchronize(var1D);
+            W2D.synchronize(var2D);
+            W3D.synchronize(var3D);
+            update();
+        }
+        
     private:
         YOCTO_DISABLE_COPY_AND_ASSIGN(Sim);
     };
